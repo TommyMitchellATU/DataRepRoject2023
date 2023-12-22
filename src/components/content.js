b195dc9bf53677626fd9105020249a2b02c469da
TommyMitchellATU <g00400162@atu.ie>
2023-12-22 10:17:18 +0000

    added new boxed layout with seperarates using days

--- src/components/content.js
+++ src/components/content.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import '../styles.css';
 
 function Content() {
   const [weather, setWeather] = useState(null);
@@ -9,9 +10,9 @@ function Content() {
     const fetchWeather = async () => {
       try {
         const response = await axios.get(
-          `https://api.weatherapi.com/v1/current.json?key=118e69e8d93b40ce92d93634232212&q=${encodeURIComponent(
+          `https://api.weatherapi.com/v1/forecast.json?key=118e69e8d93b40ce92d93634232212&q=${encodeURIComponent(
             location
-          )}`
+          )}&days=7`
         );
         setWeather(response.data);
       } catch (error) {
@@ -22,14 +23,36 @@ function Content() {
     fetchWeather();
   }, [location]);
 
+  const getDayOfWeek = (dateString) => {
+    const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    const date = new Date(dateString);
+    const dayOfWeek = date.getDay();
+    return daysOfWeek[dayOfWeek];
+  };
+
   return (
     <div className="weather-section">
       {weather ? (
         <div>
-          <h2 className="weather-heading">Current Weather</h2>
-          <p className="weather-info">Location: {weather.location.name}</p>
-          <p className="weather-info">Temperature: {weather.current.temp_c}°C</p>
-          <p className="weather-info">Condition: {weather.current.condition.text}</p>
+          <h2 className="weather-heading">Weather Forecast</h2>
+          <div className="week-container">
+            {weather.forecast && weather.forecast.forecastday ? (
+              weather.forecast.forecastday.map((day) => (
+                <div className="day-box" key={day.date}>
+                  <h3>{getDayOfWeek(day.date)}</h3>
+                  {day.day && (
+                    <>
+                      <p>Max Temperature: {day.day.maxtemp_c}°C</p>
+                      <p>Min Temperature: {day.day.mintemp_c}°C</p>
+                      <p>Condition: {day.day.condition.text}</p>
+                    </>
+                  )}
+                </div>
+              ))
+            ) : (
+              <p>No forecast data available.</p>
+            )}
+          </div>
         </div>
       ) : (
         <p>Loading weather data...</p>
